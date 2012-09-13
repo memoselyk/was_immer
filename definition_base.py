015a891dd5862e9e67f2acb1186510def9467558
Guillermo Zapata <memoselyk@somehost>
2012-09-13 02:54:48 -0500

    Download speed control on url opener

--- definition_base.py
+++ definition_base.py
@@ -1,9 +1,47 @@
 import os
 import urllib
+import time
 
 class CustomUserAgentOpener(urllib.FancyURLopener):
     version = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Ubuntu/11.04 Chromium/14.0.825.0 Chrome/14.0.825.0 Safari/535.1"
     #version = "Mozilla/5.0 (iPod; U; CPU like Mac OS X; en) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/3A101a Safari/419.3"
+
+    def __init__(self, *args, **kwargs):
+        urllib.FancyURLopener.__init__(self, *args, **kwargs)
+        self.__next_download_after = 0.0
+
+    def open(self, fullurl, data=None):
+        print 'Our very open was called'
+        #
+        # TODO: Change User-Agent 'dynamically'
+        #
+        print 'Using pre-set headers: %r' % self.addheaders
+        time_start = time.time()
+        if time_start < self.__next_download_after :
+            #
+            # Wait enough time before the next download and ...
+            time.sleep(self.__next_download_after - time_start)
+            #
+            # ... recalculate the start time
+            time_start = time.time()
+        ret = urllib.FancyURLopener.open(self, fullurl, data)
+        time_end = time.time()
+        headers = ret.info()
+        try :
+            size = int(headers["Content-Length"]) + 65 # Content + (arbitrary) size of headers
+            print 'With Length ', size
+            control_speed = 2.5 * 1024    # Speed in B/s
+            ideal_time = float(size) / control_speed
+            actual_time = time_end - time_start
+            actual_speed = float(size) / actual_time
+            print 'Download at %.3f KB/s, took %.3f instead of %.3f, next after %.3f from now' % (actual_speed/1024, actual_time, ideal_time, (time_start + ideal_time) - time.time())
+            self.__next_download_after = time_start + ideal_time
+        except Exception as reason:
+            print 'Dunno SIZE, ', reason
+            #
+            # If cannot determine the ideal download time, then assume a 500 ms wait
+            self.__next_download_after = time_end + 0.5
+        return ret
 
 #
 # Insert our customized UserAgent opener
